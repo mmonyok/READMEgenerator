cd26def0b454723925006589fb251da99f69f2f0
Ody <melodymonyok@gmail.com>
2021-06-13 18:29:02 -0500

    Added Table of Contents section.

--- markDown.js
+++ markDown.js
@@ -6,7 +6,10 @@ function generateMarkDown(data) {
 - ${data.description1}
 - ${data.description2}
 - ${data.description3}
-- ${data.description4}`;
+- ${data.description4}
+
+## Table of Contents
+- [${data.repoName}](#${data.repoName})`;
 }
 
 module.exports =  {
